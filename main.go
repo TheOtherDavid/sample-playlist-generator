a68228cb8378cb602576873b10480b5a5ec3cee3
David <davidhand1988@gmail.com>
2022-06-14 22:44:16 -0400

    Add search for artist feature.

--- main.go
+++ main.go
@@ -12,22 +12,26 @@ func main() {
 }
 
 func GeneratePlaylist() {
-	//We need to take in some artists, but let's just hardcode one for now.
-	var artists []string
-	artists = append(artists, "Rammstein")
-	fmt.Println(artists[0])
-	//How do we get from the string Rammstein to the ID?
-	var artistIds []string
-	//Just hardcode this for now.
-	artistIds = append(artistIds, "6wWVKhxIU2cEi0K81v7HvP")
-
-	var playlistName = "Sample Playlist"
-	fmt.Println(playlistName)
 
 	fmt.Println("Refreshing access token.")
 	accessToken := RefreshSpotifyAuth()
 	fmt.Println("Access token refreshed.")
 	fmt.Println(accessToken)
+
+	//We need to take in some artists, but let's just hardcode one for now.
+	var artistNames []string
+	artistNames = append(artistNames, "Rammstein")
+	fmt.Println(artistNames[0])
+	//How do we get from the string Rammstein to the ID?
+	var artistIds []string
+	//Just hardcode this for now.
+	for _, artistName := range artistNames {
+		artistId := SearchForArtistId(artistName, accessToken)
+		artistIds = append(artistIds, artistId)
+	}
+
+	var playlistName = "Sample Playlist"
+	fmt.Println(playlistName)
 
 	for _, artistId := range artistIds {
 		//Call top tracks. Put tracks into a map by album, to get the most popular track per album from the top tracks.
@@ -71,6 +75,58 @@ func RefreshSpotifyAuth() string {
 
 	accessToken := responseBody.AccessToken
 	return accessToken
+}
+
+type SpotifySearchResponse struct {
+	Artists SpotifyArtist `json:"artists"`
+}
+
+type SpotifyArtist struct {
+	Items []SpotifyArtistItem `json:"items"`
+}
+
+type SpotifyArtistItem struct {
+	Genres     []string `json:"genres"`
+	Id         string   `json:"id"`
+	Name       string   `json:"name"`
+	Popularity int      `json:"popularity"`
+	Type       string   `json:"type"`
+}
+
+func SearchForArtistId(artistName string, accessToken string) string {
+
+	q := artistName
+	url := "https://api.spotify.com/v1/search?q=" + q + "&type=artist"
+
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		//Handle Error
+	}
+
+	req.Header = http.Header{
+		"Authorization": {"Bearer " + accessToken},
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Oh no, error.")
+	}
+
+	/*response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Oh no, error.")
+	} else {
+		fmt.Println(response)
+	}*/
+	var responseBody SpotifySearchResponse
+	err = json.NewDecoder(response.Body).Decode(&responseBody)
+	if err != nil {
+		fmt.Println("Oh no, error.")
+	}
+	//Assume the first result is correct and take that ID.
+	artistId := responseBody.Artists.Items[0].Id
+	return artistId
 }
 
 func GetTopTrackIds(artistId string, accessToken string) []string {
